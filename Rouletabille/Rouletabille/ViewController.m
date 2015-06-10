c5ee182c0063e7a243feccfba2c823447dee7558
Ancil <ancilm2000@yahoo.com>
2015-06-10 09:08:30 +0200

    Rouletabille: Add starfish and collision detection with ball

--- Rouletabille/Rouletabille/ViewController.m
+++ Rouletabille/Rouletabille/ViewController.m
@@ -12,12 +12,17 @@
 static const NSTimeInterval updateInterval = 0.01; //100 Hz
 static const CGFloat acc_deadband_threshold = 0.02;
 static const CGFloat acc_max = 1.0;
+static NSString* const kStarfishBoundaryIdentifier = @"starfish";
 
-@interface ViewController ()
+@interface ViewController () <UICollisionBehaviorDelegate>
+
 @property (nonatomic,strong) CMMotionManager* cmmanager;
 @property (nonatomic,strong) UIDynamicAnimator* animator;
 @property (nonatomic,strong) UIGravityBehavior* gravity;
 @property (nonatomic,strong) UICollisionBehavior* boundary;
+@property (nonatomic,strong) UIImageView* ball;
+@property (nonatomic,strong) UIImageView* starfish;
+
 @end
 
 @implementation ViewController
@@ -64,26 +69,68 @@ static const CGFloat acc_max = 1.0;
     }
     
     //add movable object, centered in the view
-    UIImageView* ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bille"]];
-    CGRect ballframe = ball.frame;
+    self.ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bille"]];
+    CGRect ballframe = self.ball.frame;
     CGPoint ballcenter = [self.view center];
     ballframe.origin.x =  ballcenter.x-ballframe.size.width/2;
     ballframe.origin.y = ballcenter.y-ballframe.size.height/2;
-    ball.frame = ballframe;
-    [self.view addSubview:ball];
+    self.ball.frame = ballframe;
+    [self.view addSubview:self.ball];
+    
+    //add starfish target
+    self.starfish = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"etoile-96"]];
+    self.starfish.frame = [self randomStarfishFrame];
+    [self.view addSubview:self.starfish];
     
     //UI Kit Dynamics
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    self.gravity = [[UIGravityBehavior alloc] initWithItems:@[ball]];
+    self.gravity = [[UIGravityBehavior alloc] initWithItems:@[self.ball]];
     self.gravity.magnitude = 0;
     self.gravity.angle = 0;
     
-    self.boundary = [[UICollisionBehavior alloc] initWithItems:@[ball]];
+    self.boundary = [[UICollisionBehavior alloc] initWithItems:@[self.ball]];
     self.boundary.translatesReferenceBoundsIntoBoundary = YES;
+
+    [self.boundary addBoundaryWithIdentifier:kStarfishBoundaryIdentifier
+                                     forPath:[self starfishCollisionBoundary]];
+    self.boundary.collisionDelegate = self;
     
     [self.animator addBehavior:self.gravity];
     [self.animator addBehavior:self.boundary];
     
+}
+
+-(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(NSString*)identifier atPoint:(CGPoint)p
+{
+
+    if ( [identifier isEqualToString:kStarfishBoundaryIdentifier] ){
+        [self.boundary removeBoundaryWithIdentifier:kStarfishBoundaryIdentifier];
+        self.starfish.frame = [self randomStarfishFrame];
+        [self.boundary addBoundaryWithIdentifier:kStarfishBoundaryIdentifier
+                                         forPath:[self starfishCollisionBoundary]];
+    }
+    else {
+        //play sound
+    }
+    
+}
+
+
+# pragma mark - Helper functions
+-(CGRect)randomStarfishFrame;
+{
+    CGRect frame = self.starfish.frame;
+    CGPoint origin = CGPointMake( arc4random_uniform(self.view.frame.size.width-frame.size.width),
+                                  arc4random_uniform(self.view.frame.size.height-frame.size.height));
+    frame.origin = origin;
+    return frame;
+}
+
+-(UIBezierPath*)starfishCollisionBoundary;
+{
+    CGRect rect = CGRectInset(self.starfish.frame, self.starfish.frame.size.width*0.25,
+                              self.starfish.frame.size.width*0.25);
+    return [UIBezierPath bezierPathWithRect:rect];
 }
 
 -(BOOL)prefersStatusBarHidden;
