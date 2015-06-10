04d9b9494adb56ede2910bd443ba36e1e6384692
Ancil <ancilm2000@yahoo.com>
2015-06-10 11:56:14 +0200

    Rouletabille: Add audio * Add background audio, as well as audio when contact with the walls and with the star

--- Rouletabille/Rouletabille/ViewController.m
+++ Rouletabille/Rouletabille/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import <CoreMotion/CoreMotion.h>
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 #import "ViewController.h"
 
 static const NSTimeInterval updateInterval = 0.01; //100 Hz
@@ -14,7 +16,7 @@ static const CGFloat acc_deadband_threshold = 0.02;
 static const CGFloat acc_max = 1.0;
 static NSString* const kStarfishBoundaryIdentifier = @"starfish";
 
-@interface ViewController () <UICollisionBehaviorDelegate>
+@interface ViewController () <UICollisionBehaviorDelegate, AVAudioPlayerDelegate>
 
 @property (nonatomic,strong) CMMotionManager* cmmanager;
 @property (nonatomic,strong) UIDynamicAnimator* animator;
@@ -22,13 +24,31 @@ static NSString* const kStarfishBoundaryIdentifier = @"starfish";
 @property (nonatomic,strong) UICollisionBehavior* boundary;
 @property (nonatomic,strong) UIImageView* ball;
 @property (nonatomic,strong) UIImageView* starfish;
-
+@property (nonatomic,strong) AVAudioPlayer* backgroundAudioPlayer;
+@property (nonatomic,strong) AVAudioPlayer* wallBumpAudioPlayer;
+@property (nonatomic,strong) AVAudioPlayer* starBumpAudioPlayer;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //start background music on background queue
+    dispatch_queue_t dispatchQueue =
+    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    dispatch_async(dispatchQueue, ^(void){
+        
+        self.backgroundAudioPlayer = [self audioPlayerWithSound:@"midnight-ride-01a"];
+        if ([self.backgroundAudioPlayer prepareToPlay]){
+            [self.backgroundAudioPlayer play];
+        }
+
+        self.wallBumpAudioPlayer = [self audioPlayerWithSound:@"son"];
+        self.starBumpAudioPlayer = [self audioPlayerWithSound:@"squeeze-toy-1"];
+        
+    });
     
     //core motion manager for acceleration data
     self.cmmanager = [[CMMotionManager alloc] init];
@@ -90,6 +110,11 @@ static NSString* const kStarfishBoundaryIdentifier = @"starfish";
     
     self.boundary = [[UICollisionBehavior alloc] initWithItems:@[self.ball]];
     self.boundary.translatesReferenceBoundsIntoBoundary = YES;
+    
+    //add a bit of elasticity to the ball to add bounce
+    UIDynamicItemBehavior* itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.ball]];
+    itemBehavior.elasticity = 0.5;
+    [self.animator addBehavior:itemBehavior];
 
     [self.boundary addBoundaryWithIdentifier:kStarfishBoundaryIdentifier
                                      forPath:[self starfishCollisionBoundary]];
@@ -98,25 +123,39 @@ static NSString* const kStarfishBoundaryIdentifier = @"starfish";
     [self.animator addBehavior:self.gravity];
     [self.animator addBehavior:self.boundary];
     
+
 }
+
+#pragma mark - CollisionBehavior Methods
 
 -(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(NSString*)identifier atPoint:(CGPoint)p
 {
 
     if ( [identifier isEqualToString:kStarfishBoundaryIdentifier] ){
+        
+        if (self.starBumpAudioPlayer != nil){
+            if ([self.starBumpAudioPlayer prepareToPlay]){
+                [self.starBumpAudioPlayer play];
+            }
+        }
+        
         [self.boundary removeBoundaryWithIdentifier:kStarfishBoundaryIdentifier];
         self.starfish.frame = [self randomStarfishFrame];
         [self.boundary addBoundaryWithIdentifier:kStarfishBoundaryIdentifier
                                          forPath:[self starfishCollisionBoundary]];
     }
     else {
-        //play sound
+        if (self.wallBumpAudioPlayer != nil){
+        }
+            if ([self.wallBumpAudioPlayer prepareToPlay]){
+                [self.wallBumpAudioPlayer play];
+        }
+        else {
+            NSLog(@"WallBumpAudioPlayer is nil");
+        }
     }
-    
 }
 
-
-# pragma mark - Helper functions
 -(CGRect)randomStarfishFrame;
 {
     CGRect frame = self.starfish.frame;
@@ -133,6 +172,31 @@ static NSString* const kStarfishBoundaryIdentifier = @"starfish";
     return [UIBezierPath bezierPathWithRect:rect];
 }
 
+
+#pragma mark - Audio Player methods
+
+
+-(AVAudioPlayer*)audioPlayerWithSound:(NSString*)sound;
+{
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* path = [bundle pathForResource:sound ofType:@"mp3"];
+    NSData* data = [NSData dataWithContentsOfFile:path];
+    NSError* error;
+    AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithData:data error:&error];
+    if (error != nil || player == nil){
+        NSLog(@"Error initializing audio player");
+    }else{
+        player.delegate = self;
+    }
+    return player;
+}
+
+-(void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag;
+{
+    
+}
+
+# pragma mark - Helper functions
 -(BOOL)prefersStatusBarHidden;
 {
     return YES;
